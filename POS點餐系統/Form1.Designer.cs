476587e8c154e869bcb4342e0f7a3bd44077b842
Dexter <9270123a@gmail.com>
2024-09-10 14:58:16 +0800

    加入專案檔案。

---
+++ POS點餐系統/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace POS點餐系統
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.foodresultLB = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(22, 55);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(237, 277);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(365, 55);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(237, 277);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(22, 388);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(237, 277);
+            this.flowLayoutPanel3.TabIndex = 4;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(365, 388);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(237, 277);
+            this.flowLayoutPanel4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(723, 628);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "總金額";
+            // 
+            // foodresultLB
+            // 
+            this.foodresultLB.AutoSize = true;
+            this.foodresultLB.Location = new System.Drawing.Point(793, 628);
+            this.foodresultLB.Name = "foodresultLB";
+            this.foodresultLB.Size = new System.Drawing.Size(11, 12);
+            this.foodresultLB.TabIndex = 7;
+            this.foodresultLB.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "主餐";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(363, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "附餐";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "飲料";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(363, 362);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "甜點";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 687);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.foodresultLB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label foodresultLB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ POS點餐系統/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace POS點餐系統
+{
+    public partial class Form1 : Form
+    {
+        string[] foods = { "雞腿飯$70", "排骨飯$90", "控肉飯$100" };
+        string[] sidemeal = { "茶碗蒸$30", "滷豆腐$10", "滷蛋$10" };
+        string[] drink = { "奶茶$30", "紅茶$20", "可樂$40" };
+        string[] dessert = { "蛋糕$80", "布丁$90", "麵包$40" };
+
+
+
+        int Result;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateCheckBox(foods, flowLayoutPanel1);
+            GenerateCheckBox(sidemeal, flowLayoutPanel2);
+            GenerateCheckBox(drink, flowLayoutPanel3);
+            GenerateCheckBox(dessert, flowLayoutPanel4);
+
+        }
+
+
+        private void GenerateCheckBox(string[] foods, FlowLayoutPanel flow)
+        {
+     
+            for(int i = 0; i < foods.Count(); i++)
+            {
+                FlowLayoutPanel Childflow  = new FlowLayoutPanel();
+                CheckBox checkBox = new CheckBox();
+                NumericUpDown numericUpDown = new NumericUpDown();
+                checkBox.Text = foods[i];
+                checkBox.Click += CheckBox_Click;
+                numericUpDown.ValueChanged += numericUpdown_ValueChanged;
+                //checkBox.Tag = numericUpDown;
+                //numericUpDown.Tag = checkBox;
+                Childflow.Controls.Add(checkBox);
+                Childflow.Controls.Add(numericUpDown);
+                flow.Controls.Add(Childflow);
+
+
+            }
+
+        }
+
+        private void numericUpdown_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            var flow = numericUpDown.Parent;
+            var check = (CheckBox)flow.Controls[0];
+            string[] price = check.Text.Split('$');
+            int foodPrice = int.Parse(price[1]);
+
+            
+            
+
+            //if (numericupdown.value > 0 )
+            //{
+            //    check.checked = true;
+            //}
+            //else
+            //{
+            //    check.checked = false;
+            //}
+            //if (check.checked == true)
+            //{
+            //    result += (int)numericupdown.value * foodprice;
+            //}
+            //else
+            //{
+            //    result -= 1 * foodprice;
+            //}
+            //foodresultlb.text = result.tostring();
+        }
+        private void CheckBox_Click(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            var flow = checkBox.Parent;
+            var c = (NumericUpDown)flow.Controls[1];
+            var d = c.Value;
+            int quality = (int)d;
+
+            string[] price = checkBox.Text.Split('$');
+            int foodPrice = int.Parse(price[1]);
+
+            if (checkBox.Checked)
+            {
+                quality = 1;
+                c.Value = quality;
+                Result += foodPrice* quality;
+            }
+            else
+            {
+                quality = (int)c.Value;
+                c.Value = 0;
+                Result -= foodPrice * quality;
+            }
+
+            foodresultLB.Text = Result.ToString();
+        }
+
+        private void ResultCalculate()
+        {
+
+        }
+
+
+        // 
+
+    }
+}
